9016fd386ad37a0d1c44950d678812f331192325
David Geisert <dvdgeisert@gmail.com>
2022-08-08 11:37:53 -0700

    Making name unique

--- api/models.py
+++ api/models.py
@@ -4,9 +4,8 @@ from django.urls import reverse
 # Create your models here.
 
 class Account(models.Model):
-    name = models.CharField(max_length = 200)
-    balance = models.DecimalField(decimal_places=2, max_digits=9)
-    slug = models.SlugField(null=True)
+    name = models.CharField(max_length = 200, unique=True)
+    balance = models.DecimalField(decimal_places=2, max_digits=9)    
 
     def __str__(self):
         return "{n}: {b}".format(n=self.name, b=self.balance)
