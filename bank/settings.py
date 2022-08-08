998a04408bfa5dfc9bfbed383ef159f16ee28358
David Geisert <dvdgeisert@gmail.com>
2022-08-08 12:52:05 -0700

    Adding Docker

--- bank/settings.py
+++ bank/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6!7uml+wuxbb6fs1jtkzc^u5=i_$o&2nkg1x4_o@2$ya=kc2z*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
